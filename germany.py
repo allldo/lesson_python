9a5d73a228eb21442477f254cbc04fe60cff778d
allldo <dotan2.maks@mail.ru>
2021-11-10 23:11:23 +0600

    akeqwkqw[r

--- germany.py
+++ germany.py
@@ -4,11 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import json
-
+from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 delay = 7
 def get_page_data(driver, url):
+    some_data = []
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36"
+    }
     # myElem = WebDriverWait(driver.get(url), delay)
     driver.get(url)
     driver.implicitly_wait(10)
@@ -16,19 +20,45 @@ def get_page_data(driver, url):
     button.click()
     driver.implicitly_wait(35)
     elems = driver.find_elements(By.CLASS_NAME, 'bt-open-in-overlay')
-    counter = 0
+    hrefs = []
+    counter = 1
     for x in elems:
-        print(x.get_attribute('href'))
-        # print(counter)
-        counter+=1
-    
+        hrefs.append(x.get_attribute('href'))
+    for url in hrefs:
 
+        req = requests.get(url, headers)
+        with open(f"data1/{counter}.html", "w", encoding="utf-8") as file:
+            file.write(req.text)
+
+        with open(f"data1/{counter}.html", encoding="utf-8") as file:
+            src = file.read()
+
+        soup = BeautifulSoup(src, "lxml")
+        try:
+            name = soup.find('div', class_='col-xs-8 col-md-9 bt-biografie-name').text
+        except:
+            name = 'noname'
+        try:
+            position = soup.find('div', class_='bt-biografie-beruf').text
+        except:
+            position = 'posss none'
+        try:
+            links = [].append(soup.find('a', class_='bt-link-extern').text)
+        except:
+            links = []
+            links.append('empty link')
+
+        some_data.append(
+            {
+                'Name': name,
+                'Position': position,
+                'links': links,
+            }
+        )
+        counter+=1
+        driver.implicitly_wait(35)
     with open("bundes_pussies.json", "w", encoding="utf-8") as file:
-        json.dump(elems, file, indent=4, ensure_ascii=False)
-    # lines = driver.find_element_by_tag_name("h3")
-    # for x in lines:
-    #     print(x.text)
-    # lines = lines.find_element_by_class_name("piwikTrackContent piwikContentIgnoreInteraction")
+        json.dump(some_data, file, indent=4, ensure_ascii=False)
 
 
 def main():
